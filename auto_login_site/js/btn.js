ab047aeb6b80a4d2f83fa40c20044d4ffc015c17
rayee-github <32994498+rayee-github@users.noreply.github.com>
2019-08-13 17:33:00 +0800

    Add files via upload

---
+++ auto_login_site/js/btn.js
@@ -0,0 +1,11 @@
+﻿if(location.hostname=="member.ithome.com.tw")
+{
+	var element = document.getElementById('account');
+	element.value="a1";
+
+	var element = document.getElementById('password');
+	element.value="a2";
+
+	var click = document.getElementsByClassName("btn btn-agree btn-margin")[0].click();
+	console.log(click);
+}
---
+++ auto_login_site/js/popup.js
@@ -0,0 +1,8 @@
+﻿window.onload=function (){
+	//chrome.tabs.executeScript(null, {file: "js/delete_img.js"});
+	document.getElementById("btn").onclick=function(){
+		chrome.tabs.executeScript(null, {file: "js/btn.js"});
+	};
+};
+
+
